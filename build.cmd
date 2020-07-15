1e9f70e5970308898ec007df43efbd7af045a40a
chuacw <1757930+chuacw@users.noreply.github.com>
2020-07-15 15:07:47 +0800

    Updated build.cmd
    Add another way to pass -ldflags

--- build.cmd
+++ build.cmd
@@ -3,6 +3,7 @@ SET GOROOT=k:\Development\Go
 SET PATH=K:\Development\Go\bin
 go build -o HelloWorld1.exe -ldflags "-X 'main.Version=1.15' -X \"main.MyGreetings=What's going on\" -v 
 go build -o HelloWorld2.exe -ldflags "-X 'main.Version=1.18' -X \"main.MyGreetings=Hello world!\" -v  
+go build -o HelloWorld3.exe -ldflags "-X 'main.Version=1.18' -X 'main.MyGreetings=This is fantastic!'" -v  
 HelloWorld1
 HelloWorld2
-
+HelloWorld3
